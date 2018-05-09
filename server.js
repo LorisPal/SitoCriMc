2213a9da5c068f689b790fb058288633e0146d3e
LorisPalmucci <generalle913@gmail.com>
2018-05-09 19:17:11 +0200

    aggiunta/modifica pagine

--- server.js
+++ server.js
@@ -14,14 +14,44 @@ app.get('/', function(req, res){
 app.get('/sostienici', function(req, res){
     res.render('sostienici/sostienici');
 });
-app.get('/chiSiamo', function(req, res){
-    res.render('chiSiamo/chiSiamo');
+app.get('/comitato', function(req, res){
+    res.render('chiSiamo/comitato');
+});
+app.get('/storia', function(req, res){
+    res.render('chiSiamo/storia');
+});
+app.get('/principi', function(req, res){
+    res.render('chiSiamo/principi');
+});
+app.get('/diu', function(req, res){
+    res.render('chiSiamo/diu');
 });
 app.get('/contatti', function(req, res){
     res.render('contatti/contatti');
 });
-app.get('/cosaFacciamo', function(req, res){
-    res.render('cosaFacciamo/cosaFacciamo');
+app.get('/corsiFormazione', function(req, res){
+    res.render('contatti/corsiFormazione');
+});
+app.get('/diventaVolontario', function(req, res){
+    res.render('contatti/diventaVolontario');
+});
+app.get('/richiediAmbulanza', function(req, res){
+    res.render('contatti/richiediAmbulanza');
+});
+app.get('/emergenza', function(req, res){
+    res.render('cosaFacciamo/emergenza');
+});
+app.get('/giovaniSviluppo', function(req, res){
+    res.render('cosaFacciamo/giovaniSviluppo');
+});
+app.get('/principiValori', function(req, res){
+    res.render('cosaFacciamo/principiValori');
+});
+app.get('/salute', function(req, res){
+    res.render('cosaFacciamo/salute');
+});
+app.get('/sociale', function(req, res){
+    res.render('cosaFacciamo/sociale');
 });
 app.get('/lavoraConNoi', function(req, res){
     res.render('lavoraConNoi/lavoraConNoi');
