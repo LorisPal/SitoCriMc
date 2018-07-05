e0a6b1bf9a034fdae65b9a2a8b7f7e4efefccd87
loris <generalle913@gmail.com>
2018-07-05 18:04:07 +0200

    Aggiunta di varie funzionalità relative all'aministratore

--- module/sqlite.js
+++ module/sqlite.js
@@ -1,44 +1,233 @@
 const sqlite3 = require('sqlite3').verbose();
-const databse = './sitoCri.db';
+const database = './sitoCri.db';
 
 module.exports = {
-    getNotizie: function(callback) {
-        let db = new sqlite3.Database('./sitoCri.db');
+    getNotizie: function (callback) {
+        let db = new sqlite3.Database(database);
         let news = [];
-        let sql = 'select * from NEWS';
+        let sql = 'select * from NEWS ORDER BY ID DESC';
 
-        db.all(sql, [], (err,row) => {
-            if(err) {
+        db.all(sql, [], (err, row) => {
+            if (err) {
                 throw err;
             }
 
-        row.forEach((row) => {
-            var notizie = {};
-            notizie.titolo = row.TITOLO;
-            notizie.testo = row.TESTO;
+            row.forEach((row) => {
+                var notizie = {};
+                notizie.titolo = row.TITOLO;
+                notizie.testo = row.TESTO;
+                notizie.data = row.DATA;
 
-            news.push(notizie)
+                news.push(notizie)
+            });
+
+            callback(news)
         });
 
-        callback(news)
-        });
-
-    db.close();
+        db.close();
     },
 
-    setNotizie: function(a,b) { //callback in caso di lettura dati da un componente da inviare poi al post????
-        let db = new sqlite3.Database('./sitoCri.db');
-        let news = {};
-      
-        let sql = `INSERT INTO NEWS(TITOLO,TESTO) VALUES(?,?)`;
-        db.run(sql, [a,b], function(err){
-            if(err) {
+    setNotizie: function (titolo, testo, data) { //callback in caso di lettura dati da un componente da inviare poi al post????
+        let db = new sqlite3.Database(database);
+        let sql = `INSERT INTO NEWS(TITOLO,TESTO,DATA) VALUES(?,?,?)`;
+        db.run(sql, [titolo, testo, data], function (err) {
+            if (err) {
                 throw err;
-                console.log('errore di connessione al db')
+                console.log('errore di connessione al db');
             }
 
-            console.log('inserimento dati corretto')
+            console.log('inserimento dati corretto');
         });
-    db.close();
+        db.close();
+    },
+
+    setProgrammati: function (nomePz, cognomePz, prestazione, inizio, fine, indirizzoPartenza, indirizzoArrivo) {
+        let db = new sqlite3.Database(database);
+
+        let sql = `INSERT INTO PROGRAMMATI(NOMEPZ,COGNOMEPZ,PRESTAZIONE,INIZIO,FINE,INDIRIZZOP,INDIRIZZOA) VALUES(?,?,?,?,?,?,?)`;
+
+        db.run(sql, [nomePz, cognomePz, prestazione, inizio, fine, indirizzoPartenza, indirizzoArrivo], function (err) {
+            if (err) {
+                throw err;
+                console.log('dati non inseriti');
+            }
+
+            console.log('inserimento dati corretto');
+        });
+        db.close();
+    },
+
+    getRichieste: function (callback) {
+        let db = new sqlite3.Database(database);
+        let richieste = [];
+        let richieste2 = [];
+        let richieste3 = [];
+        let courses = [];
+        let sql = `SELECT * FROM PROGRAMMATI`;
+        let sql2 = `SELECT * FROM EVENTO`;
+        let sql3 = `SELECT * FROM TAXI`
+        let sql4 = `SELECT * FROM COURSE`
+
+        db.all(sql, [], (err, row) => {
+            if (err) {
+                throw err;
+            }
+
+            row.forEach((row) => {
+                var dati = {};
+                dati.nome = row.NOMEPZ;
+                dati.cognome = row.COGNOMEPZ;
+                dati.prestazione = row.PRESTAZIONE;
+                dati.oraInizio = row.INIZIO;
+                dati.oraFine = row.FINE;
+                dati.partenza = row.INDIRIZZOP;
+                dati.destinazione = row.INDIRIZZOA;
+
+                richieste.push(dati);
+            });
+        });
+
+        db.all(sql2, [], (err, row) => {
+            if (err) {
+                throw err;
+            }
+
+            row.forEach((row) => {
+                var dati2 = {};
+                dati2.evento = row.TIPOLOGIA;
+                dati2.luogo = row.LUOGO;
+                dati2.indirizzo = row.INDIRIZZO;
+                dati2.oraInizio = row.INIZIO;
+                dati2.oraFine = row.FINE;
+
+                richieste2.push(dati2);
+            });
+        });
+
+        db.all(sql3, [], (err, row) => {
+            if (err) {
+                throw err;
+            }
+
+            row.forEach((row) => {
+                var dati3 = {};
+                dati3.nome = row.NOMEPZ;
+                dati3.cognome = row.COGNOMEPZ;
+                dati3.partenza = row.INDIRIZZOP;
+                dati3.destinazione = row.INDIRIZZOA;
+                dati3.oraInizio = row.INIZIO;
+                dati3.oraFine = row.FINE;
+
+                richieste3.push(dati3);
+            });
+        });
+
+        db.all(sql4, [], (err, row) => {
+            if (err) {
+                throw err;
+            }
+
+            row.forEach((row) => {
+                var dati4 = {};
+                dati4.Id = row.ID;
+                dati4.nomeCorso = row.NOMEC;
+                dati4.istruttore = row.ISTRUTTORE;
+                dati4.inizio = row.INIZIO;
+                dati4.requisiti = row.REQUISITI;
+                dati4.descrizione = row.DESCRIZIONE;
+
+                courses.push(dati4);
+            });
+
+            callback(richieste, richieste2, richieste3, courses)
+        });
+
+        db.close();
+    },
+
+    setEvento: function (tipologia, luogo, indirizzo, inizio, fine) {
+        let db = new sqlite3.Database(database);
+
+        let sql = `INSERT INTO EVENTO(TIPOLOGIA,LUOGO,INDIRIZZO,INIZIO,FINE) VALUES(?,?,?,?,?)`;
+
+        db.run(sql, [tipologia, luogo, indirizzo, inizio, fine], function (err) {
+            if (err) {
+                throw err;
+                console.log('dati non inseriti');
+            }
+
+            console.log('inserimento dati corretto');
+        });
+        db.close();
+    },
+
+    setTaxi: function (nomePz, cognomePz, inizio, fine, indirizzoPartenza, indirizzoArrivo) {
+        let db = new sqlite3.Database(database);
+
+        let sql = `INSERT INTO TAXI(NOMEPZ,COGNOMEPZ,INIZIO,FINE,INDIRIZZOP,INDIRIZZOA) VALUES(?,?,?,?,?,?)`;
+
+        db.run(sql, [nomePz, cognomePz, inizio, fine, indirizzoPartenza, indirizzoArrivo], function (err) {
+            if (err) {
+                //throw err;
+                console.log('dati non inseriti');
+            }
+
+            console.log('inserimento dati corretto');
+        });
+        db.close();
+    },
+
+    setCourse: function (nomeCorso, istruttore, inizio, requisiti, descrizione) {
+        let db = new sqlite3.Database(database);
+        let sql = `INSERT INTO COURSE(NOMEC, ISTRUTTORE, INIZIO, REQUISITI, DESCRIZIONE) VALUES (?,?,?,?,?)`;
+        db.run(sql, [nomeCorso, istruttore, inizio, requisiti, descrizione], function (err) {
+            if (err) {
+                console.log("Dati non inseriti");
+            };
+
+            console.log("Dati Inseriti");
+        });
+        db.close();
+    },
+
+    getCourse: function (callback) {
+        let db = new sqlite3.Database(database);
+        let course = [];
+        let sql = 'select * from COURSE';
+
+        db.all(sql, [], (err, row) => {
+            if (err) {
+                throw err;
+            }
+
+            row.forEach((row) => {
+                var list = {};
+                list.nomeCorso = row.NOMEC;
+                list.istruttore = row.ISTRUTTORE;
+                list.inizio = row.INIZIO;
+                list.requisiti = row.REQUISITI;
+                list.descrizione = row.DESCRIZIONE;
+
+                course.push(list)
+            });
+
+            callback(course)
+        });
+
+        db.close();
+    },
+
+    delCourse: function(id) {
+        let db = new sqlite3.Database(database);
+        let sql = `DELETE FROM COURSE WHERE ROWID=?`
+
+        db.run(sql,[id], function(err) {
+            if (err) {
+                consol.log('Dati non cancellati');
+            };
+
+            console.log('Dati canellati');
+        });
+        db.close();
     }
 }
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var sqllite = require("./module/sqlite.js");
-'use strict';
 const nodemailer = require('nodemailer');
 
 
@@ -18,13 +17,18 @@ app.use(express.static(__dirname + '/img')); //contiene tutte le immagini
 
 
 /* 
-Porzione di codice adibita a creare una sessione di login amministratore autenticata 
+Crea una sessione di login amministratore autenticata 
 */
 app.get('/', function (req, res) {
     res.render('home/home1');
 });
 
 const admin_user = {
+    user: "", //inserire una mail valida
+    pass: ""  //inserire una password
+}
+
+const local_user = {
     user: "", //inserire una mail valida
     pass: ""  //inserire una password
 }
@@ -72,50 +76,182 @@ app.post('/amministrazioneLogin', function (req, res) {
 sezione dedidicata all'utente amministratore con vari servizi disponibili 
 */
 app.get('/amministrazione', checkAuthentication, function (req, res) {
-    res.render('home/amministrazione');
+    sqllite.getRichieste(function (richieste, richieste2, richieste3, courses) {
+        res.render('home/amministrazione', {
+            "richieste_ambulanza": richieste,
+            "richieste_ambulanza2": richieste2,
+            "richieste_ambulanza3": richieste3,
+            "richieste_corsi": courses
+        });
+        console.log(richieste, richieste2, richieste3, courses);
+    });
 });
+
 //logout dalla sessione
 app.post('/', function (req, res) {
     req.session = null;
-    console.log("session close")
+    console.log("session close");
     res.redirect('/');
 });
-//scrittura su db
+
+/* 
+Sezione Database
+*/
+
+//News: scrittura Db
 app.post('/notizie/new', function (req, res) {
-    console.log("sending data...")
+    console.log("sending data...");
     titolo = req.body.titolo;
     testo = req.body.testo;
+    data = req.body.data;
     console.log(titolo, testo)
-    sqllite.setNotizie(titolo, testo);
+    sqllite.setNotizie(titolo, testo, data);
+    res.redirect("/amministrazione");
 });
 
+app.post('/home/amministrazione', function (req, res) {
+    console.log("Sending Data...");
+    nomec = req.body.nomec;
+    istruttore = req.body.istruttore
+    inizio = req.body.inizio;
+    requisiti = req.body.requisiti;
+    descrizione = req.body.descrizione;
+    console.log(nomec, istruttore, requisiti, descrizione);
+    sqllite.setCourse(nomec, istruttore, inizio, requisiti, descrizione)
+    res.redirect("/amministrazione");
+});
+
+var rimuovi = function (req, res, next) {
+        bind = req.body.binding;
+        next();
+    }
+app.post('/home/amministrazione/rimuovi',rimuovi, function (req, res) {
+    sqllite.delCourse(bind);
+    console.log(bind);
+    res.redirect("/amministrazione");
+});
+
+app.post('/home/aggiungi', function (req, res) {
+    console.log("aggiungi");
+    
+});
+
+
+//News: lettura Db
+app.get('/new', function (req, res, next) {
+    sqllite.getNotizie(function (news) {
+        res.render('notizie/new', {
+            "notizie_cri": news
+        });
+    });
+});
+
+app.get('/corsiFormazione', function (req, res, next) {
+    sqllite.getCourse(function (course) {
+        res.render('contatti/corsiFormazione', {
+            "richieste_corsi": course
+        });
+    });
+});
+
+
+
+/* 
+Sezione Utenza
+*/
+
+//Invio richiesta ambulanza
 app.post('/contatti/richiediAmbulanza', function (req, res) {
+    console.log("sending data...")
+
     //select different template
     var selettore = req.body.trasporto;
+    console.log(selettore);
     switch (selettore) {
-        case "programmato":
+
+        case "Programmato":
+            nome = req.body.nome;
+            cognome = req.body.cognome;
+            prestazione = req.body.prestazione;
+            oraInizio = req.body.oraInizio;
+            oraFine = req.body.oraFine;
+            partenza = req.body.partenza;
+            destinazione = req.body.destinazione;
+            console.log(nome, cognome, prestazione, oraInizio, oraFine, partenza, destinazione)
+            sqllite.setProgrammati(nome, cognome, prestazione, oraInizio, oraFine, partenza, destinazione);
+
             template = `
         <h1>Croce Rossa Macerata</h1>
-        <h3>Modulo richiesta Ambulanza Programmato</h3>
+        <h3>Modulo richiesta Trasporto ${selettore}</h3>
         <ul>
             <li>Nome: ${req.body.nome}</li>
             <li>Cognome: ${req.body.cognome}</li>
             <li>Prestazione: ${req.body.prestazione}</li>
-            <li>Orari: ${req.body.ora}</li>
+            <li>Orari: Inizio: ${req.body.oraInizio} - Fine: ${req.body.oraFine}</li>
             <li>Struttura/Indirizzo Partenza: ${req.body.partenza}</li>
             <li>Struttura/Indirizzo Destinazione: ${req.body.destinazione}</li>
         </ul>`
             break;
 
-        case "sportivo":
+        case "Sportivo":
+            evento = req.body.evento;
+            luogo = req.body.luogo;
+            indirizzo = req.body.indirizzo;
+            oraInizio = req.body.oraInizio;
+            oraFine = req.body.oraFine;
+            console.log(evento, luogo, indirizzo, oraInizio, oraFine)
+            sqllite.setEvento(evento, luogo, indirizzo, oraInizio, oraFine);
+
+
             template = `
         <h1>Croce Rossa Macerata</h1>
-        <h3>Modulo richiesta Ambulanza Sportiva</h3>
+        <h3>Modulo richiesta Evento ${selettore}</h3>
         <ul>
             <li>Evento Sportivo: ${req.body.evento}</li>
             <li>Luogo: ${req.body.luogo}</li>
-            <li>Struttura Ospitante: ${req.body.partenza}</li>
-            <li>Orari: ${req.body.ora}</li>
+            <li>Struttura Ospitante: ${req.body.indirizzo}</li>
+            <li>Orari: Inizio: ${req.body.oraInizio} - Fine: ${req.body.oraFine}</li>
+        </ul>`
+            break;
+
+        case "Manifestazione":
+            evento = req.body.evento;
+            luogo = req.body.luogo;
+            indirizzo = req.body.indirizzo;
+            oraInizio = req.body.oraInizio;
+            oraFine = req.body.oraFine;
+            console.log(evento, luogo, indirizzo, oraInizio, oraFine)
+            sqllite.setEvento(evento, luogo, indirizzo, oraInizio, oraFine);
+
+            template = `
+        <h1>Croce Rossa Macerata</h1>
+        <h3>Modulo richiesta Evento ${selettore}</h3>
+        <ul>
+            <li>Evento Sportivo: ${req.body.evento}</li>
+            <li>Luogo: ${req.body.luogo}</li>
+            <li>Struttura Ospitante: ${req.body.indirizzo}</li>
+            <li>Orari: Inizio: ${req.body.oraInizio} - Fine: ${req.body.oraFine}</li>
+        </ul>`
+            break;
+
+        case "Taxi Sanitario":
+            nome = req.body.nome;
+            cognome = req.body.cognome;
+            oraInizio = req.body.oraInizio;
+            oraFine = req.body.oraFine;
+            partenza = req.body.partenza;
+            destinazione = req.body.destinazione;
+            console.log(nome, cognome, oraInizio, oraFine, partenza, destinazione)
+            sqllite.setTaxi(nome, cognome, oraInizio, oraFine, partenza, destinazione);
+
+            template = `
+        <h1>Croce Rossa Macerata</h1>
+        <h3>Modulo richiesta Trasporto ${selettore}</h3>
+        <ul>
+            <li>Nome: ${req.body.nome}</li>
+            <li>Cognome: ${req.body.cognome}</li>
+            <li>Partenza: ${req.body.partenza} - Ora: ${req.body.oraInizio}</li>
+            <li>Arrivo: ${req.body.destinazione} - Ora: ${req.body.oraFine}</li>
         </ul>`
             break;
 
@@ -125,8 +261,8 @@ app.post('/contatti/richiediAmbulanza', function (req, res) {
 
     // create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport({
-        host: '', //smtp.provider_email
-        port: provider.port,
+        host: 'smtp.gmail.com', //smtp.provider_email
+        port: 465,
         SSL: true, //see the provider smtp configuration
         auth: {
             user: '', //account user email
@@ -137,8 +273,8 @@ app.post('/contatti/richiediAmbulanza', function (req, res) {
     // setup email data with unicode symbols
     let mailOptions = {
         from: '', // sender address
-        to: '', // list of receivers
-        subject: '', // Subject line
+        to: '', // list of receivers k.naki94@gmail.com
+        subject: 'richiesta trasporto', // Subject line
         text: '', // plain text body
         html: template // html body
     };
@@ -151,24 +287,9 @@ app.post('/contatti/richiediAmbulanza', function (req, res) {
         console.log('Message sent: %s', info.messageId);
         // Preview only available when sending through an Ethereal account
         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        console.log(selettore);
-
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     });
 });
 
-
-/* 
-sezione dedicata alla visualizzazione delle notizie pubblicate dall'amministratore 
-*/
-app.get('/new', function (req, res) {
-    sqllite.getNotizie(function (news) {
-        res.render('notizie/new', {
-            "notizie_cri": news
-        });
-    });
-});
 
 app.get('/sostienici', function (req, res) {
     res.render('sostienici/sostienici');
